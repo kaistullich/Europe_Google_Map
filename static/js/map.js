a4958c1600f19a0368ceae95ff3dc7c6fd67d06f
kaistullich <kairstullich@gmail.com>
2017-02-13 17:49:55 -0800

    placed all instances into initMap

--- static/js/map.js
+++ static/js/map.js
@@ -5,15 +5,16 @@ function EuropeMarkers(lat, lng){
 
 }
 
-var berlin = new EuropeMarkers(51.1657, 10.4515);
-var prague = new EuropeMarkers(52.520, 13.4050);
+var berlin = new EuropeMarkers(52.5200, 13.4050);
+var prague = new EuropeMarkers(50.0755, 14.4378);
 var munich = new EuropeMarkers(48.1351, 11.5820);
 var villach = new EuropeMarkers(46.6086, 13.8506);
 var venice = new EuropeMarkers(45.4408, 12.3155);
 var lake_garda = new EuropeMarkers(45.6049, 10.6351);
 
 var map;
-// var europe = {lat: 51.1657, lng: 10.4515};
+var europe = {lat: 51.1657, lng: 10.4515};
+
 // var berlin = {lat: 52.5200, lng: 13.4050};
 // var prague = {lat: 50.0755, lng: 14.4378};
 // var munich = {lat: 48.1351, lng: 11.5820};
@@ -25,32 +26,35 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: europe,
-        zoom: 6
+        zoom: 5
     });
     var berlin_marker = new google.maps.Marker({
-        position: berlin,
+        position: {lat: berlin.lat, lng: berlin.lng},
         map: map
     });
 
     var prague_marker = new google.maps.Marker({
-        position: prague,
+        position: {lat: prague.lat, lng: prague.lng},
         map: map
     });
+
     var munich_marker = new google.maps.Marker({
-        position: munich,
+        position: {lat: munich.lat, lng: munich.lng},
         map: map
     });
 
     var villach_marker = new google.maps.Marker({
-        position: villach,
+        position: {lat: villach.lat, lng: villach.lng},
         map: map
     });
+
     var venice_marker = new google.maps.Marker({
-        position: venice,
+        position: {lat: venice.lat, lng: venice.lng},
         map: map
     });
+
     var gardaLake_marker = new google.maps.Marker({
-        position: lake_garda,
+        position: {lat: lake_garda.lat, lng: lake_garda.lng},
         map: map
     });
 }
