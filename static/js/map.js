c4c70a83ffdad0108834ad93ee30396952837290
kaistullich <kairstullich@gmail.com>
2017-02-13 19:24:22 -0800

    added information windows for all markers

--- static/js/map.js
+++ static/js/map.js
@@ -18,33 +18,144 @@ function initMap() {
         center: europe,
         zoom: 5
     });
+
+
+
+
+    // Start Berlin
     var berlin_marker = new google.maps.Marker({
         position: {lat: berlin.lat, lng: berlin.lng},
         map: map
     });
 
+    var berlinContent = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Berlin</h1>';
+
+    var berlin_infowindow = new google.maps.InfoWindow({
+        content: berlinContent
+    });
+
+    berlin_marker.addListener('click', function() {
+        berlin_infowindow.open(map, berlin_marker);
+    });
+    // END Berlin
+
+
+
+
+
+    // Start Prague
     var prague_marker = new google.maps.Marker({
         position: {lat: prague.lat, lng: prague.lng},
         map: map
     });
 
+    var pragueContent = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">Prague</h1>';
+
+    var prague_infowindow = new google.maps.InfoWindow({
+        content: pragueContent
+    });
+
+    prague_marker.addListener('click', function() {
+        prague_infowindow.open(map, prague_marker);
+    });
+    // END Prague
+
+
+
+
+
+    // Start Munich
     var munich_marker = new google.maps.Marker({
         position: {lat: munich.lat, lng: munich.lng},
         map: map
     });
 
+    var munichContent = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Munich</h1>';
+
+    var munich_infowindow = new google.maps.InfoWindow({
+        content: munichContent
+    });
+
+    munich_marker.addListener('click', function() {
+        munich_infowindow.open(map, munich_marker);
+    });
+    // END Munich
+
+
+
+
+
+    // Start Villach
     var villach_marker = new google.maps.Marker({
         position: {lat: villach.lat, lng: villach.lng},
         map: map
     });
 
+    var villachContent = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Villach</h1>';
+
+    var villach_infowindow = new google.maps.InfoWindow({
+        content: villachContent
+    });
+
+    villach_marker.addListener('click', function() {
+        villach_infowindow.open(map, villach_marker);
+    });
+    // END Villach
+
+
+
+
+
+    // Start Venice
     var venice_marker = new google.maps.Marker({
         position: {lat: venice.lat, lng: venice.lng},
         map: map
     });
+    var veniceContent = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Venice</h1>';
 
+    var venice_infowindow = new google.maps.InfoWindow({
+        content: veniceContent
+    });
+
+    venice_marker.addListener('click', function() {
+        venice_infowindow.open(map, venice_marker);
+    });
+    // END Venice
+
+
+
+
+    // Start Garda Lake
     var gardaLake_marker = new google.maps.Marker({
         position: {lat: lake_garda.lat, lng: lake_garda.lng},
         map: map
     });
+    var gardaContent = '<div id="content">'+
+        '<div id="siteNotice">'+
+        '</div>'+
+        '<h1 id="firstHeading" class="firstHeading">Lake Garda</h1>';
+
+    var garda_infowindow = new google.maps.InfoWindow({
+        content: gardaContent
+    });
+
+    gardaLake_marker.addListener('click', function() {
+        garda_infowindow.open(map, gardaLake_marker);
+    });
+    // END Garda Lake
 }
