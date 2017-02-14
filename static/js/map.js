3f3bdb7d0105956e95ded2efeda380ac5fef05d7
kaistullich <kairstullich@gmail.com>
2017-02-13 17:36:40 -0800

    created constructor for EuropeMarkers, and instances for all cities

--- static/js/map.js
+++ static/js/map.js
@@ -1,19 +1,29 @@
 /**
  * Created by kaistullich on 2/13/17.
  */
-// TODO create class (constructor) EuropeMarkers
-function EuropeMarkers(){
+// TODO finish class (constructor) EuropeMarkers
+function EuropeMarkers(lat, lng){
+    this.lat = lat;
+    this.lng = lng;
 
 }
 
+var berlin = new EuropeMarkers(51.1657, 10.4515);
+var prague = new EuropeMarkers(52.520, 13.4050);
+var munich = new EuropeMarkers(48.1351, 11.5820);
+var villach = new EuropeMarkers(46.6086, 13.8506);
+var venice = new EuropeMarkers(45.4408, 12.3155);
+var lake_garda = new EuropeMarkers(45.6049, 10.6351);
+
 var map;
-var europe = {lat: 51.1657, lng: 10.4515};
-var berlin = {lat: 52.5200, lng: 13.4050};
-var prague = {lat: 50.0755, lng: 14.4378};
-var munich = {lat: 48.1351, lng: 11.5820};
-var villach = {lat: 46.6086, lng: 13.8506};
-var venice = {lat: 45.4408, lng: 12.3155};
-var lake_garda = {lat: 45.6049, lng: 10.6351};
+// var europe = {lat: 51.1657, lng: 10.4515};
+// var berlin = {lat: 52.5200, lng: 13.4050};
+// var prague = {lat: 50.0755, lng: 14.4378};
+// var munich = {lat: 48.1351, lng: 11.5820};
+// var villach = {lat: 46.6086, lng: 13.8506};
+// var venice = {lat: 45.4408, lng: 12.3155};
+// var lake_garda = {lat: 45.6049, lng: 10.6351};
+
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
