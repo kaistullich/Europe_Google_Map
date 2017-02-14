a3742d1ded33e2b575bd28ba194afb107e270e99
kaistullich <kairstullich@gmail.com>
2017-02-13 18:56:00 -0800

    removed all old var for cities

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,6 @@
-// TODO finish class (constructor) EuropeMarkers
 function EuropeMarkers(lat, lng){
     this.lat = lat;
     this.lng = lng;
-
 }
 
 var berlin = new EuropeMarkers(52.5200, 13.4050);
@@ -14,14 +12,6 @@ var lake_garda = new EuropeMarkers(45.6049, 10.6351);
 
 var map;
 var europe = {lat: 51.1657, lng: 10.4515};
-
-// var berlin = {lat: 52.5200, lng: 13.4050};
-// var prague = {lat: 50.0755, lng: 14.4378};
-// var munich = {lat: 48.1351, lng: 11.5820};
-// var villach = {lat: 46.6086, lng: 13.8506};
-// var venice = {lat: 45.4408, lng: 12.3155};
-// var lake_garda = {lat: 45.6049, lng: 10.6351};
-
 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
